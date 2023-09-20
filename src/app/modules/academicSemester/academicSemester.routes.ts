8cc9e8b3b93f647fb9e97b75d52e8ec3bb184c23
sarwar-asik <sarwarasik@gmail.com>
2023-09-20 16:11:10 +0600

    validation academic semester

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidation } from './academicSemesterValidatio';
 
 const router = express.Router();
 
-router.post('/', AcademicSemesterController.insertIntoDb);
+router.post('/',validateRequest(AcademicSemesterValidation.create), AcademicSemesterController.insertIntoDb);
 router.get('/', AcademicSemesterController.getAllFromDb);
 
 router.get('/:id', AcademicSemesterController.getSingleFromDb);
 
-router.patch('/:id', AcademicSemesterController.updateOneDb);
+router.patch('/:id',validateRequest(AcademicSemesterValidation.update), AcademicSemesterController.updateOneDb);
 router.delete('/:id', AcademicSemesterController.deleteFromDb);
 
 export const academicSemesterRoutes = router;
