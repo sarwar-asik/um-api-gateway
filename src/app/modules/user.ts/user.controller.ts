c0b383e8d76a16bcba63a9e22087b36d32c30a02
sarwar-asik <sarwarasik@gmail.com>
2023-09-22 00:29:35 +0600

    cloudinary first upload

--- src/app/modules/user.ts/user.controller.ts
+++ src/app/modules/user.ts/user.controller.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
+import { fileUploadHelper } from '../../../helpers/FileUploadHelpers';
 
 
 
 const createStudent = async (req: Request, res: Response, next: NextFunction) => {
-    
+    console.log("from create-user controller", req?.body);
+    fileUploadHelper.uploadToCloudinary()
+
 };
 
 
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { academicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
+import { userRouter } from '../modules/user.ts/user.router';
 
 const router = express.Router();
 
@@ -7,7 +8,11 @@ const moduleRoutes = [
   {
     path: '/academic-semester',
     routes: academicSemesterRoutes
-  }
+  },
+  {
+    path: '/users',
+    routes: userRouter
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route?.routes));
