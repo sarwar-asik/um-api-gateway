4df108a3bf0364769f0c6709f02c0e6cc3551285
sarwar-asik <sarwarasik@gmail.com>
2023-09-23 01:02:37 +0600

    multa and cloudinary type declrad

--- src/app/modules/user.ts/user.service.ts
+++ src/app/modules/user.ts/user.service.ts
@@ -1,11 +1,14 @@
 import { Request } from 'express';
 import { fileUploadHelper } from '../../../helpers/FileUploadHelpers';
+import { IUploadFile, IcloudinaryResponse } from '../../../interfaces/fileUpload';
 
 const createStudent = async (req: Request) => {
   console.log(req.file, 'files');
-  const file = req.file;
-  const uploadedImage = await fileUploadHelper.uploadToCloudinary(file)
+  const file = req.file as IUploadFile
 
+  const uploadedImage :IcloudinaryResponse= await fileUploadHelper.uploadToCloudinary(file)
+  console.log(uploadedImage);
+  
   return uploadedImage
   
 };
--- src/helpers/FileUploadHelpers.ts
+++ src/helpers/FileUploadHelpers.ts
@@ -1,6 +1,7 @@
 import {v2 as cloudinary} from 'cloudinary';
 import multer from 'multer';
 import * as fs from "fs"
+import { IUploadFile, IcloudinaryResponse } from '../interfaces/fileUpload';
 
 
 cloudinary.config({ 
@@ -9,7 +10,7 @@ cloudinary.config({
   api_secret: 'G8SWNZe7qnQR0lIyGe_adrFkN9A' 
 });
 
-// !multer 
+// !multer (for upload file in folder )
 
 const storage = multer.diskStorage({
   destination:function(req,file,cb){
@@ -26,16 +27,17 @@ const upload = multer({storage:storage})
 
 // ! cloudinary ///
 
-const uploadToCloudinary =async(file:any)=>{
+const uploadToCloudinary =async(file:IUploadFile):Promise<IcloudinaryResponse>=>{
   //   cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
   // { public_id: "olympic_flag" }, 
   // function(error, result) {console.log(result); });
 
   return new Promise((resolve,reject)=>{
     cloudinary.uploader.upload(file.path,
-    { public_id: file.originalname }, 
-    (error,result)=>{
-      fs.unlinkSync(file.path)
+    // { public_id: file.originalname },  //! don't need the line otherwise will type error
+    
+    (error:Error,result:IcloudinaryResponse)=>{
+      fs.unlinkSync(file.path)   ///! unlinkSync means delete file
       if(error){
         reject(error)
       }else{
