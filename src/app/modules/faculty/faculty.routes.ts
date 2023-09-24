a79ad11445f48505074d439ad05902f1921a1a3a
sarwar-asik <sarwarasik@gmail.com>
2023-09-24 23:36:30 +0600

    faculties validation

--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import auth from '../../middlewares/auth';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import { FacultyController } from './faculty.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { FacultyValidation } from './faculty.validation';
 
 const router = express.Router();
 
-router.post('/:id', auth(ENUM_USER_ROLE.SUPER_ADMIN,ENUM_USER_ROLE.ADMIN), FacultyController.updateOneDb);
+router.patch('/:id',validateRequest(FacultyValidation.updateFacultyZodSchema), auth(ENUM_USER_ROLE.SUPER_ADMIN,ENUM_USER_ROLE.ADMIN), FacultyController.updateOneDb);
 
 export const facultyRouter = router;
