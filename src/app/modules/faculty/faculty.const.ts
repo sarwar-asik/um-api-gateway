23919ea424bec08dd7b74735651d19a0c87c4672
sarwar-asik <sarwarasik@gmail.com>
2023-09-25 13:36:40 +0600

    complete api-getway

--- src/app/modules/faculty/faculty.const.ts
+++ src/app/modules/faculty/faculty.const.ts
@@ -1,2 +1,2 @@
-export const EVENT_FACULTY_CREATED = 'faculty.created'
-export const EVENT_FACULTY_UPDATED = 'faculty.updated'
+export const EVENT_FACULTY_CREATED = 'faculty.created';
+export const EVENT_FACULTY_UPDATED = 'faculty.updated';
